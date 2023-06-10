7755c3e5f398737073a2d39760a3151be980c0f0
птичка <Sapunova270301@gmail.com>
2023-06-10 20:49:36 +0300

    change src/

---
+++ src/test/java/ru/netology/DebitCardPositiveTest.java
@@ -0,0 +1,52 @@
+package ru.netology;
+
+        import io.github.bonigarcia.wdm.WebDriverManager;
+        import org.junit.jupiter.api.AfterEach;
+        import org.junit.jupiter.api.BeforeAll;
+        import org.junit.jupiter.api.BeforeEach;
+        import org.junit.jupiter.api.Test;
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.chrome.ChromeDriver;
+        import org.openqa.selenium.chrome.ChromeOptions;
+
+        import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DebitCardPositiveTest {
+    private WebDriver driver;
+
+
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void TestAllInput() {
+        driver.get("http://localhost:9999/");
+        WebElement form = driver.findElement(By.cssSelector("[class] form"));
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Коваль Алиса");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79990000000");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("[type=button]")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals(expected, actual);
+    }
+}
